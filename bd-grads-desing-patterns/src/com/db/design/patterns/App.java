fddc9fd4f49a6d3014844f0bfd5fc69f1ec9df74
vamandeshmukh <vamandeshmukh@gmail.com>
2020-09-13 22:18:56 +0530

    Please clone / download this repo for hands-on.

---
+++ bd-grads-desing-patterns/src/com/db/design/patterns/App.java
@@ -0,0 +1,23 @@
+package com.db.design.patterns;
+
+/**
+ * 
+ * @author Vaman Deshmukh
+ * @version 0.0.1.SNAPSHOT
+ */
+public class App {
+
+	/**
+	 * 
+	 * This app is meant to implement most common design patterns. Create separate
+	 * package for each design pattern. Adhere to industry-standard coding
+	 * practices.
+	 * 
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to DB!");
+		
+		// Invoke methods here
+	}
+
+}
